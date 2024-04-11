6e90b1252598e093387597b3e2c4e54bf799356e
guskirb <anguskirby1998@gmail.com>
2024-04-12 00:40:17 +0100

    Refactor to use classes

--- java.js
+++ java.js
@@ -11,46 +11,107 @@ let authorValue = "";
 let genreValue = "";
 let readValue = false;
 
-titleInput.onchange = titleVal;
-authorInput.onchange = authorVal;
-genreInput.onchange = genreVal;
-readInput.onchange = readVal;
-
-function titleVal() {
-    titleValue = titleInput.value.toUpperCase();
+class Book {
+    constructor(name, author, genre, read) {
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+        this.read = read;
+    }
 };
 
-function authorVal() {
-    authorValue = authorInput.value.toUpperCase();
+class Library {
+    constructor() {
+        this.library = [];
+    }
+
+    addBook(title) {
+        this.library.push(title);
+    }
+    notRead(x) {
+        this.library[x].read = false;
+        page.refreshShelf();
+    }
+    read(x) {
+        this.library[x].read = true;
+        page.refreshShelf();
+    }
+    changeRead(x) {
+        if (this.library[x].read === true) {
+            this.library[x].read = false;
+        } if (this.library[x].read === false) {
+            this.library[x].read = true;
+        };
+        page.refreshShelf();
+    }
+    addBookToLibrary(title, author, genre, read) {
+        title = new Book(title, author, genre, read);
+        this.library.push(title);
+    }
 };
 
-function genreVal() {
-    genreValue = genreInput.value.toUpperCase();
+class Render {
+    refreshShelf() {
+        this.clearShelf();
+        for (let x = 0; x < myLibrary.library.length; x++) {
+            bookShelf.appendChild(document.createElement("div"));
+            bookShelf.lastChild.appendChild(document.createElement("button")).classList.add(x);
+            bookShelf.lastChild.appendChild(document.createElement("li")).textContent = myLibrary.library[x].name;
+            bookShelf.lastChild.appendChild(document.createElement("li")).textContent = myLibrary.library[x].author;
+            bookShelf.lastChild.appendChild(document.createElement("li")).textContent = myLibrary.library[x].genre;
+            this.readButton(x);
+            bookShelf.lastChild.firstChild.addEventListener("click", function () {
+                myLibrary.library.splice(x, 1);
+                bookShelf.removeChild(bookShelf.lastChild);
+            });
+        }
+    }
+    clearShelf() {
+        while (bookShelf.hasChildNodes()) {
+            bookShelf.removeChild(bookShelf.lastChild);
+        }
+    }
+    readButton(x) {
+        if (myLibrary.library[x].read === true) {
+            bookShelf.lastChild.appendChild(document.createElement("button")).setAttribute("id", x);
+            document.getElementById(x).classList.add("read");
+            document.getElementById(x).addEventListener("click", () => {
+                myLibrary.library[x].read = false;
+                this.refreshShelf();
+            });
+        } else if (myLibrary.library[x].read === false) {
+            bookShelf.lastChild.appendChild(document.createElement("button")).setAttribute("id", x)
+            document.getElementById(x).classList.add("notRead");
+            document.getElementById(x).addEventListener("click", () => {
+                myLibrary.library[x].read = true;
+                this.refreshShelf();
+            })
+        }
+    }
+    titleVal() {
+        titleValue = titleInput.value.toUpperCase();
+    }
+
+    authorVal() {
+        authorValue = authorInput.value.toUpperCase();
+    }
+
+    genreVal() {
+        genreValue = genreInput.value.toUpperCase();
+    }
+
+    readVal() {
+        readValue = readInput.checked;
+    }
 };
 
-function readVal() {
-    readValue = readInput.checked;
-};
-
-
-const myLibrary = [];
-
-function Book(name, author, genre, read) {
-    this.name = name;
-    this.author = author;
-    this.genre = genre;
-    this.read = read;
-};
-
-function addBookToLibrary(title, author, genre, read) {
-    title = new Book(title, author, genre, read);
-    myLibrary.push(title);
-};
+const myLibrary = new Library;
+const page = new Render;
 
 button.addEventListener("click", function (event) {
     event.preventDefault();
-    addBookToLibrary(titleValue, authorValue, genreValue, readValue);
-    refreshShelf();
+    myLibrary.addBookToLibrary(titleValue, authorValue, genreValue, readValue);
+    page.refreshShelf();
     titleInput.value = "";
     titleValue = "";
     authorInput.value = "";
@@ -61,63 +122,7 @@ button.addEventListener("click", function (event) {
     readValue = false;
 });
 
-function clearShelf() {
-    while (bookShelf.hasChildNodes()) {
-        bookShelf.removeChild(bookShelf.lastChild);
-    }
-};
-
-function notRead(x) {
-    myLibrary[x].read = false;
-    refreshShelf();
-}
-
-function read(x) {
-    myLibrary[x].read = true;
-    refreshShelf();
-}
-
-function changeRead(x) {
-    if (myLibrary[x].read === true) {
-        myLibrary[x].read = false;
-    } if (myLibrary[x].read === false) {
-        myLibrary[x].read = true;
-    };
-    refreshShelf();
-};
-
-function refreshShelf() {
-    clearShelf();
-    for (let x = 0; x < myLibrary.length; x++) {
-        bookShelf.appendChild(document.createElement("div"));
-        bookShelf.lastChild.appendChild(document.createElement("button")).classList.add(x);
-        bookShelf.lastChild.appendChild(document.createElement("li")).textContent = myLibrary[x].name;
-        bookShelf.lastChild.appendChild(document.createElement("li")).textContent = myLibrary[x].author;
-        bookShelf.lastChild.appendChild(document.createElement("li")).textContent = myLibrary[x].genre;
-        readButton(x);
-        bookShelf.lastChild.firstChild.addEventListener("click", function () {
-            myLibrary.splice(x, 1);
-            bookShelf.removeChild(bookShelf.lastChild);
-        });
-
-
-    };
-}
-
-function readButton(x) {
-    if (myLibrary[x].read === true) {
-        bookShelf.lastChild.appendChild(document.createElement("button")).setAttribute("id", x);
-        document.getElementById(x).classList.add("read");
-        document.getElementById(x).addEventListener("click", function () {
-            myLibrary[x].read = false;
-            refreshShelf();
-        });
-    } else if (myLibrary[x].read === false) {
-        bookShelf.lastChild.appendChild(document.createElement("button")).setAttribute("id", x)
-        document.getElementById(x).classList.add("notRead");
-        document.getElementById(x).addEventListener("click", function () {
-            myLibrary[x].read = true;
-            refreshShelf();
-        });
-    };
-}
+titleInput.onchange = page.titleVal;
+authorInput.onchange = page.authorVal;
+genreInput.onchange = page.genreVal;
+readInput.onchange = page.readVal;
